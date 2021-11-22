cd9d8a2a38035bde3c3117281472ef789acacebc
Benjamin <75579412+devbaze@users.noreply.github.com>
2021-11-22 12:28:55 +0100

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,15 +16,15 @@ function Portfolio() {
 			<h3 className="section-title">Portfolio</h3>
 			<div className="spacer" data-height="30"></div>
       
-			{<ul className="portfolio-filter list-inline">
+			<ul className="portfolio-filter list-inline">
 				<li className="current list-inline-item" data-filter="*">All Projects</li>
 				<li className="list-inline-item" data-filter=".branding">Branding</li>
 				<li className="list-inline-item" data-filter=".creative">Creative</li>
 				<li className="list-inline-item" data-filter=".design">Design</li>
 				<li className="list-inline-item" data-filter=".video">Video</li>
-			</ul>}
+			</ul>
       
-			{<div className="pf-filter-wrapper mb-4">
+			<div className="pf-filter-wrapper mb-4">
 				<select className="portfolio-filter-mobile">
 					<option value="*">Everything</option>
 					<option value=".creative">Creative</option>
@@ -32,7 +32,7 @@ function Portfolio() {
 					<option value=".design">Design</option>
 					<option value=".branding">Branding</option>
 				</select>
-			</div>}
+			</div>
       
 			<div className="row portfolio-wrapper">
         <WorkItem tags="Art, Creative" title="Creative Art" image="image" />
@@ -44,14 +44,14 @@ function Portfolio() {
 
 			</div>
       
-			{<div className="load-more text-center mt-4">
+			<div className="load-more text-center mt-4">
 				<a href="javascript:" className="btn btn-default"><i className="fas fa-circle-notch"></i> Load more</a>
         
 				<ul className="portfolio-pagination list-inline d-none">
 					<li className="list-inline-item">1</li>
 					<li className="list-inline-item"><a href="works-2.html">2</a></li>
 				</ul>
-			</div>}
+			</div>
 		</section>
     
 		
